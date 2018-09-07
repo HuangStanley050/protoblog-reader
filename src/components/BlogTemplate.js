829972ae397a17d46426fae737fcd1cf2bd59380
Stanley Huang <infamous_godhand@yahoo.com>
2018-09-07 04:54:21 +0000

    able to get the content of the blog post

--- src/components/BlogTemplate.js
+++ src/components/BlogTemplate.js
@@ -4,11 +4,21 @@ import { connect } from "react-redux";
 import { toggle } from "../store/actions/toggle";
 
 const Template = (props) => {
+    let content = [];
     return (
         <div className={style.Modal}>
             
             <div className={style.Content}>
             <button className={style.button} onClick={props.toggle}>Close Post</button>
+            {props.posts.map(post=>{
+                if(props.ID===post.id){
+                    //return <h3>{post.content[0].text}</h3>
+                    content=post.content.map(paragraph=>{
+                        return <p>{paragraph.text}</p>;
+                    });
+                }
+            })}
+            {content}
             </div>
             </div>
     );
@@ -20,4 +30,11 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(null, mapDispatchToProps)(Template);
+const mapStateToProps = state => {
+    return {
+        posts: state.post,
+        ID: state.activeID
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Template);
